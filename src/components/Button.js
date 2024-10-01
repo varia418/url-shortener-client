787ad2a1dcfcdc218fb003e3934a3c1c14bb56eb
varia <ltnhan2610@gmail.com>
2024-10-01 10:15:44 +0700

    add input and button components

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({ label }) {
+    return (
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'>{label}</button>
+    )
+}
+
+export default Button
---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Input({ type, label }) {
+    return (
+        <div class="mb-6">
+            <label for="large-input" class="block mb-2 text-md font-medium text-gray-900">{label}</label>
+            <input type={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'></input>
+        </div>
+    )
+}
+
+export default Input
