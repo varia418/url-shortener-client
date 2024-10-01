163403b8182d9c1feb02b2cffa8d9e70ded0c85b
varia <ltnhan2610@gmail.com>
2024-10-01 12:02:38 +0700

    fix the prefix to be responsive on mobile

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ function Input({ type, label, isRequired = false, prefix = '' }) {
     return (
         <div class="mb-6">
             <label for="large-input" class="block mb-2 text-md font-medium text-gray-900">{label}</label>
-            <div class="flex gap-2 items-center">
+            <div class="flex flex-col gap-2 sm:items-center sm:flex-row">
                 {prefix && <span className='text-nowrap'>{prefix}</span>}
                 <input type={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ' required={isRequired}></input>
             </div>
