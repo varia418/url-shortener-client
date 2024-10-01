4ad0af9c9f98dcd55a33ab0f19d1b3c0aa41aba0
varia <ltnhan2610@gmail.com>
2024-10-01 10:38:50 +0700

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <Header />
-                </div>
-            </header>
-        </div>
+        <main className="container mx-auto px-4 min-h-screen flex flex-col">
+            <Header />
+            <Footer />
+        </main>
     );
 }
 
