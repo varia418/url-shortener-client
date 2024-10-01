29c53b81df45180e1f092be4eea7b75c6fed1b4f
varia <ltnhan2610@gmail.com>
2024-10-01 11:42:09 +0700

    add isRequired prop

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Input({ type, label }) {
+function Input({ type, label, isRequired = false }) {
     return (
         <div class="mb-6">
             <label for="large-input" class="block mb-2 text-md font-medium text-gray-900">{label}</label>
-            <input type={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'></input>
+            <input type={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ' required={isRequired}></input>
         </div>
     )
 }
