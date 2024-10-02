122623019719b5c224eef57104cb2bb0d7df63e4
varia <ltnhan2610@gmail.com>
2024-10-02 09:50:28 +0700

    adjust button color

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ label }) {
     return (
-        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'>{label}</button>
+        <button className='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'>{label}</button>
     )
 }
 
--- src/components/ResultSnackbar.js
+++ src/components/ResultSnackbar.js
@@ -11,7 +11,7 @@ function ResultSnackbar({ url }) {
                 <p class="flex items-center text-sm font-normal text-gray-500">{url}</p>
             </div>
             <div class="flex items-center flex-shrink-0">
-                <a href="#" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none">Copy to Clipboard</a>
+                <a href="#" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none">Copy to Clipboard</a>
                 <button data-dismiss-target="#marketing-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
