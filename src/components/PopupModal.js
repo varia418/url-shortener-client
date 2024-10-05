3d25788ed0d5fc951eeba1c9a84f3df27cf2afb8
varia <ltnhan2610@gmail.com>
2024-10-05 11:33:20 +0700

    add confirm button to popup modal

--- src/components/PopupModal.js
+++ src/components/PopupModal.js
@@ -11,11 +11,14 @@ function PopupModal({ onClose }) {
                         </svg>
                         <span className="sr-only">Close modal</span>
                     </button>
-                    <div className="p-4 md:p-5 text-center">
+                    <div className="p-4 md:p-5 text-center flex flex-col">
                         <svg className="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
                         <h3 className="mb-5 text-lg font-normal text-gray-500">This URL does not exist or has expired.</h3>
+                        <button onClick={onClose} type="button" className="self-end text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                            OK
+                        </button>
                     </div>
                 </div>
             </div>
